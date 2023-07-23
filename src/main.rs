ba9d7ab8feab11f0be72c28b711b8075733167d7
Marcus Luck <marcus.luck@outlook.com>
2023-07-23 08:39:14 -1000

    CHG: remote temp reader code

--- src/main.rs
+++ src/main.rs
@@ -42,48 +42,36 @@ bind_interrupts!(struct Irqs {
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
-    info!("Hello, world!");
+    info!("Starting up!");
     let p = embassy_rp::init(Default::default());
-
-    info!("It's about to break!");
-    // let mut pw = singleton!(pwm::Pwm::new_output_b(p.PWM_CH1, p.PIN_3, c.clone()));
 
     let pw = p.PWM_CH1;
     let pn = p.PIN_3;
-    let _led_h = spawner.spawn(blink_led(pw, pn)).unwrap();
+    let _led_h = spawner.spawn(pulse_led_1(pw, pn)).unwrap();
 
     let pw2 = p.PWM_CH2;
     let pn2 = p.PIN_4;
-    let _led_h = spawner.spawn(blink_led2(pw2, pn2)).unwrap();   
-    // let mut led_on = Output::new(p.PIN_2, Level::Low);
-    info!("Time to spawn a temp reader");
-    let sda = p.PIN_18;
-    let scl = p.PIN_19;
-    let i2c1 = p.I2C1;
-
-    spawner.spawn(read_temp(i2c1, sda, scl)).unwrap();
-
-    // loop {
-    //     Timer::after(Duration::from_millis(3000)).await;
-    // }
+    let _led_h = spawner.spawn(pulse_led_2(pw2, pn2)).unwrap();
+    info!("All tasks initialized");
 
 }
 
+/// LED pulse task 1.
+/// 
+/// This function is designed to not be generic to make it easier to adapt.
+/// It takes a specific `PWM` channel and `PIN` combination, see Pico documentation.
+/// 
 #[embassy_executor::task]
-async fn blink_led(pwm_ch1: PWM_CH1, pin3: PIN_3) {
+async fn pulse_led_1(pwm_ch1: PWM_CH1, pin3: PIN_3) {
 // async fn blink_led(pwm: &'static mut pwm::Pwm<'static, PWM_CH1>, pin: AnyPin) {
 
     let mut c: pwm::Config = Default::default();
     let mut pw = pwm::Pwm::new_output_b(pwm_ch1, pin3, c.clone());
-    // pwm.set_config(&c);
+
     c.top = 12000; //0x8000;
     c.divider = 240u16.to_fixed(); // Seems like this is implmented with a u8 as the largest divider
     c.compare_b = 8;
 
-    // let mut led = Output::new(pin, Level::Low);
-    // led.set_low();
-
-    info!("Done setting up, running loop");
     let mut going_up: bool = true;
     let mut fade: u16 = 0;
     loop {
@@ -102,8 +90,6 @@ async fn blink_led(pwm_ch1: PWM_CH1, pin3: PIN_3) {
 
         c.compare_b = (fade as f32 * fade as f32 * 12000.0f32 / 65535.0f32) as u16;
 
-        // c.compare_b = c.compare_b.rotate_left(1);
-        // c.divider;
         pw.set_config(&c);
         Timer::after(Duration::from_millis(10)).await;
     }
@@ -111,7 +97,7 @@ async fn blink_led(pwm_ch1: PWM_CH1, pin3: PIN_3) {
 
 
 #[embassy_executor::task]
-async fn blink_led2(pwm_ch2: PWM_CH2, pin4: PIN_4) {
+async fn pulse_led_2(pwm_ch2: PWM_CH2, pin4: PIN_4) {
 // async fn blink_led(pwm: &'static mut pwm::Pwm<'static, PWM_CH1>, pin: AnyPin) {
 
     let mut c: pwm::Config = Default::default();
@@ -121,10 +107,6 @@ async fn blink_led2(pwm_ch2: PWM_CH2, pin4: PIN_4) {
     c.divider = 240u16.to_fixed(); // Seems like this is implmented with a u8 as the largest divider
     c.compare_a = 8;
 
-    // let mut led = Output::new(pin, Level::Low);
-    // led.set_low();
-
-    info!("Done setting up, running loop");
     let mut going_up: bool = true;
     let mut fade: u16 = 0;
     loop {
@@ -150,23 +132,3 @@ async fn blink_led2(pwm_ch2: PWM_CH2, pin4: PIN_4) {
     }
 }
 
-#[embassy_executor::task]
-async fn read_temp(i2c1: I2C1, sda: PIN_18, scl: PIN_19) {
-
-    info!("set up i2c ");
-    let i2c = i2c::I2c::new_async(i2c1, scl, sda, Irqs, Config::default());
-
-    let mut readings = [0f32; 2];
-    // Setup i2c:
-    let mut sensor = Hygro::new(i2c);
-    sensor.init().await;
-
-    loop {
-        readings[0] = sensor.temperature().await;
-        readings[1] = sensor.humidity().await;
-        info!("temp: {} C, humidity: {}%", readings[0], readings[1]);
-
-        Timer::after(Duration::from_millis(3000)).await;
-    }
-
-}
